2c899e2ed48d5b9818d15587686b4a74eb90b05d
Paul deGrandis <paul.degrandis@gmail.com>
2017-07-27 13:38:05 -0400

    There should be a fluent API for building up the initial context

--- java/dais/Example.java
+++ java/dais/Example.java
@@ -10,12 +10,45 @@ import java.util.function.Predicate;
 
 import dais.Maps;
 import dais.Interceptor;
+import dais.Context; // Fluent API for the HashMap
 import dais.Chain;
 
 public class Example {
 
 
     public static Map<Object,Object> example() {
+        // Note: When you're actually using the Chain,
+        //       you'd be better off creating static Interceptors in a class once
+        //       and just always reusing them.
+        Map context = new Context().withTerminators(ctx -> ctx.containsKey("b"))
+                                   .withInterceptors(new Interceptor(ctx -> Maps.put(ctx, "a", 1),
+                                                                     ctx -> Maps.put(ctx, "leave-a", 11),
+                                                                     null),
+                                                     new Interceptor(ctx -> Maps.put(ctx, "b", 1),
+                                                                     null, null),
+                                                     new Interceptor(ctx -> Maps.put(ctx, "c", 1),
+                                                                     null, null));
+
+        return Chain.execute(context);
+    }
+
+    public static Map<Object,Object> example2() {
+        Map context = new Context().withTerminators(ctx -> ctx.containsKey("b"));
+
+        // Note: When you're actually using the Chain,
+        //       you'd be better off creating static Interceptors in a class once
+        //       and just always reusing them.
+        return Chain.execute(context,
+                             Arrays.asList(new Interceptor(ctx -> Maps.put(ctx, "a", 1),
+                                                           ctx -> Maps.put(ctx, "leave-a", 11),
+                                                           null),
+                                           new Interceptor(ctx -> Maps.put(ctx, "b", 1),
+                                                           null, null),
+                                           new Interceptor(ctx -> Maps.put(ctx, "c", 1),
+                                                           null, null)));
+    }
+
+    public static Map<Object,Object> example3() {
         // TODO: Make a Context class, subclass of HashMap, to improve the type inference and quiet this noise down
         Map context = Maps.mapOf("terminators",
                                 Arrays.asList((Predicate<Map<Object,Object>>) (Map<Object,Object> ctx) -> ctx.containsKey("b")));
@@ -33,7 +66,7 @@ public class Example {
                                                            null, null)));
     }
 
-    public static Map<Object,Object> example2() {
+    public static Map<Object,Object> example4() {
         // TODO: Make a Context class, subclass of HashMap, to improve the type inference and quiet this noise down
         Map context = Maps.mapOf("queue",
                                  new ArrayDeque(
